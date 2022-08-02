bceca790e0bb40474e804ae51fd54ba19b178804
Deepika <gjdeepika9@gmail.com>
2022-08-03 01:05:56 +0530

    Single line and multi line comment implemented

---
+++ src/Comments.java
@@ -0,0 +1,21 @@
+public class Comments {
+    public static void main(String[] args) {
+        //Execution begins
+        System.out.println("Single Line Comment");
+        //This line is omitted by the compiler
+        System.out.println("Yay!.. This line is executed!.");
+        Comments comments = new Comments();
+
+        //Executing a block of code with multi_line comment
+        comments.multiLineComment();
+    }
+
+    public void multiLineComment() {
+        /*
+         * Multi line comment block is omitted by the compiler
+         * Helpful in adding
+         * more description to the code
+         */
+        System.out.println("Multi Line Comment");
+    }
+}
